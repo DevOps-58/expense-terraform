2ebcf367cd344c5ceb0ff31f17ae20ed5831a40a
Girisha97 <girishavanaparthi02@gmail.com>
2024-09-11 22:44:02 -0500

    Auto push

--- main.tf
+++ main.tf
@@ -1,27 +1,27 @@
-module "frontend" {
-  depends_on = [module.backend]
+# module "frontend" {
+#   depends_on = [module.backend]
 
-  source        = "git::https://github.com/DevOps-58/tf-module-terraform.git"
-  instance_type = var.components["frontend"]["instance_type"]
-  name          = var.components["frontend"]["name"]
-  env           = var.env
-  port_no       = var.components["frontend"]["port_no"]
-  pwd           = var.pwd
-  #vault_token = var.vault_token
+#   source        = "git::https://github.com/DevOps-58/tf-module-terraform.git"
+#   instance_type = var.components["frontend"]["instance_type"]
+#   name          = var.components["frontend"]["name"]
+#   env           = var.env
+#   port_no       = var.components["frontend"]["port_no"]
+#   pwd           = var.pwd
+#   #vault_token = var.vault_token
 
-}
+# }
 
-module "backend" {
-  depends_on = [module.mysql]
+# module "backend" {
+#   depends_on = [module.mysql]
 
-  source        = "git::https://github.com/DevOps-58/tf-module-terraform.git"
-  instance_type = var.components["backend"]["instance_type"]
-  name          = var.components["backend"]["name"]
-  env           = var.env
-  port_no       = var.components["backend"]["port_no"]
-  pwd           = var.pwd
-  #vault_token = var.vault_token
-}
+#   source        = "git::https://github.com/DevOps-58/tf-module-terraform.git"
+#   instance_type = var.components["backend"]["instance_type"]
+#   name          = var.components["backend"]["name"]
+#   env           = var.env
+#   port_no       = var.components["backend"]["port_no"]
+#   pwd           = var.pwd
+#   #vault_token = var.vault_token
+# }
 
 module "mysql" {
   source        = "git::https://github.com/DevOps-58/tf-module-terraform.git"
