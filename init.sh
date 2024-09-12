8a7bbb4afe6ebe4693d409b4982b6027e65c4229
Girisha97 <girishavanaparthi02@gmail.com>
2024-09-11 20:10:37 -0500

    Auto push

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 terraform init -backend-config=env-${env}/state.tfvars
-terraform plan -var-file=env-${env}/main.tfvars  -var pwd=${"DevOps321"}
-terraform ${action} -auto-approve -var-file=env-${env}/main.tfvars -var pwd=${"DevOps321"}
+terraform plan -var-file=env-${env}/main.tfvars  -var pwd=${ssh_pwd}
+terraform ${action} -auto-approve -var-file=env-${env}/main.tfvars -var pwd=${ssh_pwd}
