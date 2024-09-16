e21358ef673e158f0465f325d1bcc25176c8ac64
Girisha97 <girishavanaparthi02@gmail.com>
2024-09-16 13:41:33 -0500

    Auto push

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 terraform init -backend-config=env-${env}/state.tfvars
-terraform plan -var-file=env-${env}/main.tfvars -var vault_token=${vault_token} 
-terraform ${action} -auto-approve -var-file=env-${env}/main.tfvars -var vault_token=${vault_token} 
+terraform plan -var-file=env-${env}/main.tfvars -var vault_token=${vault_token} -var ssh_pwd=${ssh_pwd}
+terraform ${action} -auto-approve -var-file=env-${env}/main.tfvars -var vault_token=${vault_token} -var ssh_pwd=${ssh_pwd}
